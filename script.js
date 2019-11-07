bc63c386938d2635dd354548b5fbb7618d14ed38
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-07 15:09:19 +0000

    fetch working

--- script.js
+++ script.js
@@ -1,35 +1,26 @@
-const test = document.getElementById('img');
-const buttonNewGif = document.getElementById('newGif');
+const test = document.getElementById('test');
 const buttonSubmit = document.getElementById('submit');
 const input1 = document.getElementById('input1');
 
-async function giffys() {
-  const response = await fetch(
-    'http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=3aab69399bf03eca438758bf6e33d18e',
-    {
-      mode: 'cors'
-    }
-  );
-  const gifData = await response.json();
+const getData = async id => {
+  try {
+    const response = await fetch(
+      'http://api.openweathermap.org/data/2.5/weather?id=' + id + '&appid=3aab69399bf03eca438758bf6e33d18e',
+      {
+        mode: 'cors'
+      }
+    );
+    const fetchedData = await response.json();
 
-  console.log(gifData);
-  //test.textContent = gifData.data.coord;
+    console.log(fetchedData);
 
-  response.catch(err => alert(err));
-}
-
-giffys();
-
-/*
-buttonNewGif.addEventListener('click', () => {
-  if (input1.value) {
-    giffys(input1.value);
-  } else {
-    giffys('dog');
+    test.textContent = fetchedData.name;
+  } catch (err) {
+    alert(err);
   }
-});
-*/
-buttonSubmit.addEventListener('click', () => {
-  giffys(input1.value);
-});
+};
+getData(2172797);
 
+buttonSubmit.addEventListener('click', () => {
+  getData('2172797');
+});
