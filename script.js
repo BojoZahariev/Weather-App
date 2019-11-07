1ff91244f10bddacfd55b8e6102a250e3e109bf7
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-07 14:23:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+const test = document.getElementById('img');
+const buttonNewGif = document.getElementById('newGif');
+const buttonSubmit = document.getElementById('submit');
+const input1 = document.getElementById('input1');
+
+async function giffys() {
+  const response = await fetch(
+    'http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=3aab69399bf03eca438758bf6e33d18e',
+    {
+      mode: 'cors'
+    }
+  );
+  const gifData = await response.json();
+
+  console.log(gifData);
+  //test.textContent = gifData.data.coord;
+
+  response.catch(err => alert(err));
+}
+
+giffys();
+
+/*
+buttonNewGif.addEventListener('click', () => {
+  if (input1.value) {
+    giffys(input1.value);
+  } else {
+    giffys('dog');
+  }
+});
+*/
+buttonSubmit.addEventListener('click', () => {
+  giffys(input1.value);
+});
 
