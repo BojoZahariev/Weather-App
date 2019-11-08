33c0d2a71e095da9892411155fe8a0e4bc0e42c2
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-08 10:27:02 +0000

    min/max

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const countryInput = document.getElementById('input2');
 const city = document.getElementById('city');
 const description = document.getElementById('description');
 const temp = document.getElementById('temp');
+const tempMin = document.getElementById('temp-min');
+const tempMax = document.getElementById('temp-max');
 
 const getData = async (cityName, country) => {
   try {
@@ -28,15 +30,39 @@ const getData = async (cityName, country) => {
   }
 };
 
+const get5dayData = async (cityName, country) => {
+  try {
+    const response = await fetch(
+      'http://api.openweathermap.org/data/2.5/forecast?q=' +
+        cityName +
+        ',' +
+        country +
+        '&mode=json&units=metric&appid=3aab69399bf03eca438758bf6e33d18e',
+      {
+        mode: 'cors'
+      }
+    );
+    const fetchedData = await response.json();
+
+    console.log(fetchedData);
+  } catch (err) {
+    alert(err);
+  }
+};
+
 //display the data
 const display = apiData => {
   city.textContent = apiData.name;
-  description.textContent = apiData.weather[0].description;
+  description.textContent =
+    apiData.weather[0].description.charAt(0).toUpperCase() + apiData.weather[0].description.slice(1); //capital first
   //rounded to integer
   temp.textContent = `${Math.round(apiData.main.temp)} °C`;
+  tempMin.textContent = `min: ${Math.round(apiData.main.temp_min)} °C`;
+  tempMax.textContent = `max: ${Math.round(apiData.main.temp_max)} °C`;
 };
 
 getData('leeds', 'gb');
+get5dayData('leeds', 'gb');
 
 buttonSubmit.addEventListener('click', () => {
   getData(cityInput.value, countryInput.value);
