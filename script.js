1e529cc9c1df70b4752b1ca13d75d26cbbb174ab
Bojo <bozhidar_zahariev@hotmail.com>
2019-11-12 09:16:12 +0000

    form/storage only on valid

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
-const apiKey = '3aab69399bf03eca438758bf6e33d18e';
-const buttonSubmit = document.getElementById('submit');
-const cityInput = document.getElementById('input1');
-const countryInput = document.getElementById('input2');
-const city = document.getElementById('city');
-const description = document.getElementById('description');
-const temp = document.getElementById('temp');
-const tempMin = document.getElementById('temp-min');
-const tempMax = document.getElementById('temp-max');
-const wind = document.getElementById('wind');
-const sunrise = document.getElementById('sunrise');
-const sunset = document.getElementById('sunset');
-const todayHourlyDiv = document.getElementById('today-hourly');
-const todayHourlyElement = document.getElementsByClassName('today-part');
-const forecastDiv = document.getElementById('forecast');
-const forecastElement = document.getElementsByClassName('forecast-part');
-const backToToday = document.getElementById('back-to-today');
-const validPlace = document.getElementById('validPlace');
-const closeValidPlace = document.getElementById('close');
-
-cityInput.value = 'Leeds';
-countryInput.value = 'GB';
+const apiKey = "3aab69399bf03eca438758bf6e33d18e";
+const buttonSubmit = document.getElementById("submit");
+const form = document.getElementById("form");
+const cityInput = document.getElementById("input1");
+const countryInput = document.getElementById("input2");
+const city = document.getElementById("city");
+const description = document.getElementById("description");
+const temp = document.getElementById("temp");
+const tempMin = document.getElementById("temp-min");
+const tempMax = document.getElementById("temp-max");
+const wind = document.getElementById("wind");
+const sunrise = document.getElementById("sunrise");
+const sunset = document.getElementById("sunset");
+const todayHourlyDiv = document.getElementById("today-hourly");
+const todayHourlyElement = document.getElementsByClassName("today-part");
+const forecastDiv = document.getElementById("forecast");
+const forecastElement = document.getElementsByClassName("forecast-part");
+const backToToday = document.getElementById("back-to-today");
+const validPlace = document.getElementById("validPlace");
+const closeValidPlace = document.getElementById("close");
 
 //get the data from the local storage if it's not empty
-if (localStorage.getItem('city') !== null && localStorage.getItem('country') !== null) {
-  cityInput.value = localStorage.getItem('city');
-  countryInput.value = localStorage.getItem('country');
+if (
+  localStorage.getItem("city") !== null &&
+  localStorage.getItem("country") !== null
+) {
+  cityInput.value = localStorage.getItem("city");
+  countryInput.value = localStorage.getItem("country");
 }
 
 //today's weather
@@ -33,7 +34,7 @@ const getTodayData = async (cityName, country) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${cityName},${country}&units=metric&appid=${apiKey}`,
       {
-        mode: 'cors'
+        mode: "cors"
       }
     );
     const fetchedData = await response.json();
@@ -43,7 +44,8 @@ const getTodayData = async (cityName, country) => {
     //send the data to be displayed
     displayToday(fetchedData);
   } catch (err) {
-    validPlace.style.display = 'block';
+    console.log(cityInput.value, countryInput.value);
+    validPlace.style.display = "block";
   }
 };
 
@@ -52,7 +54,7 @@ const get5dayHourlyData = async (cityName, country) => {
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/forecast?q=${cityName},${country}&units=metric&appid=${apiKey}`,
       {
-        mode: 'cors'
+        mode: "cors"
       }
     );
     const fetchedData = await response.json();
@@ -62,7 +64,7 @@ const get5dayHourlyData = async (cityName, country) => {
     getIndex(fetchedData);
     displayNext4days(fetchedData, getIndex(fetchedData));
   } catch (err) {
-    validPlace.style.display = 'block';
+    validPlace.style.display = "block";
   }
 };
 
@@ -71,7 +73,7 @@ const getIndex = apiData => {
   let newDayIndex;
 
   for (let i = 0; i < apiData.list.length; i++) {
-    if (apiData.list[i].dt_txt.includes('03:00:00')) {
+    if (apiData.list[i].dt_txt.includes("03:00:00")) {
       newDayIndex = i;
       break;
     }
@@ -81,16 +83,26 @@ const getIndex = apiData => {
 
 //display the data for Today
 const displayToday = apiData => {
+  //move the form away
+  form.classList.toggle("form-after");
+
   city.textContent = apiData.name;
   //rounded to integer
   temp.textContent = `${Math.round(apiData.main.temp)} °C`;
   description.textContent =
-    apiData.weather[0].description.charAt(0).toUpperCase() + apiData.weather[0].description.slice(1); //capital first
+    apiData.weather[0].description.charAt(0).toUpperCase() +
+    apiData.weather[0].description.slice(1); //capital first
   tempMin.textContent = `min: ${Math.round(apiData.main.temp_min)} °C`;
   tempMax.textContent = `max: ${Math.round(apiData.main.temp_max)} °C`;
   wind.textContent = `wind: ${apiData.wind.speed} m/s`;
-  sunrise.textContent = `Sunrise: ${new Date(apiData.sys.sunrise * 1000).toLocaleTimeString()}`;
-  sunset.textContent = `Sunset: ${new Date(apiData.sys.sunset * 1000).toLocaleTimeString()}`;
+  sunrise.textContent = `Sunrise: ${new Date(
+    apiData.sys.sunrise * 1000
+  ).toLocaleTimeString()}`;
+  sunset.textContent = `Sunset: ${new Date(
+    apiData.sys.sunset * 1000
+  ).toLocaleTimeString()}`;
+
+  storageItems();
 };
 
 const displayTodayHourly = (apiData, start) => {
@@ -101,21 +113,21 @@ const displayTodayHourly = (apiData, start) => {
     }
   }
   for (let i = 0; i < 8; i++) {
-    let item1 = document.createElement('p');
+    let item1 = document.createElement("p");
     todayHourlyElement[i].appendChild(item1);
     item1.textContent = apiData.list[start].dt_txt.slice(-9, -3);
 
-    let item2 = document.createElement('p');
+    let item2 = document.createElement("p");
     todayHourlyElement[i].appendChild(item2);
     item2.textContent = `${Math.round(apiData.list[start].main.temp)} °C`;
 
-    let item3 = document.createElement('p');
+    let item3 = document.createElement("p");
     todayHourlyElement[i].appendChild(item3);
     item3.textContent =
       apiData.list[start].weather[0].description.charAt(0).toUpperCase() +
       apiData.list[start].weather[0].description.slice(1); //capital first
 
-    let item4 = document.createElement('p');
+    let item4 = document.createElement("p");
     todayHourlyElement[i].appendChild(item4);
     item4.textContent = `wind: ${apiData.list[start].wind.speed} m/s`;
 
@@ -126,36 +138,37 @@ const displayTodayHourly = (apiData, start) => {
 const displayNext4days = (apiData, start) => {
   for (let i = 0; i < 4; i++) {
     forecastElement[i].textContent =
-      apiData.list[start].dt_txt.slice(8, 10) + '/' + apiData.list[start].dt_txt.slice(5, 7); //reverse the date
+      apiData.list[start].dt_txt.slice(8, 10) +
+      "/" +
+      apiData.list[start].dt_txt.slice(5, 7); //reverse the date
 
     let dateIndex = start;
 
     start += 8;
 
-    forecastElement[i].addEventListener('click', () => {
+    forecastElement[i].addEventListener("click", () => {
       displayTodayHourly(apiData, dateIndex);
     });
   }
 };
 
-backToToday.addEventListener('click', () => {
+backToToday.addEventListener("click", () => {
   get5dayHourlyData(cityInput.value, countryInput.value);
 });
 
-buttonSubmit.addEventListener('click', () => {
+buttonSubmit.addEventListener("click", () => {
   getTodayData(cityInput.value, countryInput.value);
   get5dayHourlyData(cityInput.value, countryInput.value);
+});
 
-  // Storage
-  if (typeof Storage !== 'undefined') {
-    localStorage.setItem('city', cityInput.value);
-    localStorage.setItem('country', countryInput.value);
+// Storage
+const storageItems = () => {
+  if (typeof Storage !== "undefined") {
+    localStorage.setItem("city", cityInput.value);
+    localStorage.setItem("country", countryInput.value);
   }
-});
+};
 
-closeValidPlace.addEventListener('click', () => {
-  validPlace.style.display = 'none';
+closeValidPlace.addEventListener("click", () => {
+  validPlace.style.display = "none";
 });
-
-getTodayData(cityInput.value, countryInput.value);
-get5dayHourlyData(cityInput.value, countryInput.value);
